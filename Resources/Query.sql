5d127fe943172400dc8ecd332fb7790aa864b267
mflas <mflascas@gmail.com>
2023-02-23 18:27:05 +1100

    90% upload just need the DB

---
+++ Resources/Query.sql
@@ -0,0 +1,12 @@
+-- Select *
+-- From contacts
+
+-- SELECT * FROM "Campaign"
+-- ORDER BY cf_id ASC 
+
+-- SELECT * FROM category
+-- ORDER BY category_id ASC 
+
+
+SELECT * FROM subcategory
+ORDER BY subcategory_id ASC 
